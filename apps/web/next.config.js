3ff94571a45c2aaeaa749c1d28054ac568a1591e
Rahul Shetty <rahul.shetty@ellipsonic.com>
2026-04-18 15:20:02 +0530

    production perfect

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = require('node:path').resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
